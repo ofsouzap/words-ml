4051e430260743ce581300a3b6d9c556997b5b19
Ofsouzap <ofsouzap@gmail.com>
2023-08-19 10:13:28 +0100

    More learn_word_rel_pos tests

--- tests/unit/test_learn_word_rel_pos.py
+++ tests/unit/test_learn_word_rel_pos.py
@@ -1,28 +1,61 @@
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Iterable
 import numpy as np
 import pytest
 from learn import learn_word_rel_pos
 from text_source import RawTextSource
 
 
-_CASES: List[Tuple[str, Dict[Tuple[str, str], Tuple[float, float]], int]] = [
+_CASES: List[Tuple[str, Iterable[Tuple[Tuple[str, str], Tuple[float, float]]], int]] = [
     (
         "word more more more",
-        {
-            ("word", "word"): (np.inf, np.inf),
-            ("word", "more"): (2, 2),
-            ("more", "word"): (2, -2),
-            ("more", "more"): (4/3, 0)
-        },
+        [
+            (("word", "word"), (np.inf, np.inf)),
+            (("word", "more"), (2, 2)),
+            (("more", "word"), (2, -2)),
+            (("more", "more"), (4/3, 0)),
+        ],
         5
+    ),
+    (
+        "word more more more",
+        [
+            (("word", "word"), (np.inf, np.inf)),
+            (("word", "more"), (1, 1)),
+            (("more", "word"), (1, -1)),
+            (("more", "more"), (1, 0)),
+        ],
+        1
+    ),
+    (
+        "a b c d e f",
+        [
+            (("a","a"), (np.inf,np.inf)),
+            (("a","b"), (1,1)),
+            (("b","a"), (1,-1)),
+            (("a","e"), (4,4)),
+            (("e","a"), (4,-4)),
+            (("b","d"), (2,2)),
+            (("d","b"), (2,-2)),
+        ],
+        5
+    ),
+    (
+        # From Flanders and Swann, Ill Wind
+        "i once had a whim and i had to obey it to buy a french horn in a second hand shop",
+        [
+            (("i","i"), (6,0)),
+            (("i","a"), (54/6,48/6)),
+            (("french","whim"), (10,-10)),
+        ],
+        25
     ),
     # TODO - more test cases
 ]
 """Each value has the input text and a dictionary. The dictonary values are `(unsigned_expected_value, signed_expected_value)`"""
 
 
-@pytest.mark.parametrize(("in_text", "exp_vals", "max_look_dist"), _CASES)
-def test_unsigned_symmetry(in_text: str, exp_vals: Dict[Tuple[str, str], float], max_look_dist: int):
+@pytest.mark.parametrize(("in_text", "tests", "max_look_dist"), _CASES)
+def test_unsigned_symmetry(in_text: str, tests: Dict[Tuple[str, str], float], max_look_dist: int):
 
     source = RawTextSource(in_text)
 
@@ -34,8 +67,8 @@ def test_unsigned_symmetry(in_text: str, exp_vals: Dict[Tuple[str, str], float],
                 assert np.isclose(mat[i,j], mat[j,i])
 
 
-@pytest.mark.parametrize(("in_text", "exp_vals", "max_look_dist"), _CASES)
-def test_signed_antisymmetry(in_text: str, exp_vals: Dict[Tuple[str, str], float], max_look_dist: int):
+@pytest.mark.parametrize(("in_text", "tests", "max_look_dist"), _CASES)
+def test_signed_antisymmetry(in_text: str, tests: Dict[Tuple[str, str], float], max_look_dist: int):
 
     source = RawTextSource(in_text)
 
@@ -47,31 +80,31 @@ def test_signed_antisymmetry(in_text: str, exp_vals: Dict[Tuple[str, str], float
                 assert np.isclose(mat[i,j], -mat[j,i])
 
 
-@pytest.mark.parametrize(("in_text", "exp_vals", "max_look_dist"), _CASES)
-def test_values_unsigned(in_text: str, exp_vals: Dict[Tuple[str, str], Tuple[float, float]], max_look_dist: int):
+@pytest.mark.parametrize(("in_text", "tests", "max_look_dist"), _CASES)
+def test_values_unsigned(in_text: str, tests: Dict[Tuple[str, str], Tuple[float, float]], max_look_dist: int):
 
     source = RawTextSource(in_text)
 
     mat, word_indexes = learn_word_rel_pos(source, max_look_dist=max_look_dist, signed=False)
 
-    for pair in exp_vals:
+    for pair, exp_vals in tests:
 
         i1 = word_indexes.get_to(pair[0])
         i2 = word_indexes.get_to(pair[1])
 
-        assert np.isclose(mat[i1,i2], exp_vals[pair][0])
+        assert np.isclose(mat[i1,i2], exp_vals[0]), f"Failed with {pair[0]}-{pair[1]}. Expected {exp_vals[0]}, got {mat[i1,i2]}"
 
 
-@pytest.mark.parametrize(("in_text", "exp_vals", "max_look_dist"), _CASES)
-def test_values_signed(in_text: str, exp_vals: Dict[Tuple[str, str], Tuple[float, float]], max_look_dist: int):
+@pytest.mark.parametrize(("in_text", "tests", "max_look_dist"), _CASES)
+def test_values_signed(in_text: str, tests: Dict[Tuple[str, str], Tuple[float, float]], max_look_dist: int):
 
     source = RawTextSource(in_text)
 
     mat, word_indexes = learn_word_rel_pos(source, max_look_dist=max_look_dist, signed=True)
 
-    for pair in exp_vals:
+    for pair, exp_vals in tests:
 
         i1 = word_indexes.get_to(pair[0])
         i2 = word_indexes.get_to(pair[1])
 
-        assert np.isclose(mat[i1,i2], exp_vals[pair][1])
+        assert np.isclose(mat[i1,i2], exp_vals[1]), f"Failed with {pair[0]}-{pair[1]}. Expected {exp_vals[1]}, got {mat[i1,i2]}"
